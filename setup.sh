4cad7d6a4864e56c6efa39352259816944dd4689
shamiul islam shifat <36572440+shamiulshifat@users.noreply.github.com>
2020-05-19 22:58:05 +0600

    updated

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 mkdir -p ~/.streamlit/
 echo "\
-[general]\n\
-email = \"shifat.ruet@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
